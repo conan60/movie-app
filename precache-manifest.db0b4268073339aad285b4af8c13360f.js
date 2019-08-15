dbecca3530f581f5ce4d863c52507b89759a667b
conan60 <malek.gorchene@gmail.com>
2019-08-15 16:02:03 +0100

    Updates

---
+++ precache-manifest.db0b4268073339aad285b4af8c13360f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cccd9a41285825faa5150f58ada79c92",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "085cc95f044daa19998c",
+    "url": "/movie-app/static/css/main.9ba14f66.chunk.css"
+  },
+  {
+    "revision": "6dcf2992a277d8b50523",
+    "url": "/movie-app/static/js/2.e40b15b1.chunk.js"
+  },
+  {
+    "revision": "085cc95f044daa19998c",
+    "url": "/movie-app/static/js/main.e43c660b.chunk.js"
+  },
+  {
+    "revision": "3a5315027bacbafbe345",
+    "url": "/movie-app/static/js/runtime~main.7dd63718.js"
+  },
+  {
+    "revision": "06b4c9e38f87c76345090571644bff47",
+    "url": "/movie-app/static/media/emptyStar.06b4c9e3.png"
+  }
+]);
